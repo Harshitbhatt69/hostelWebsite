4bec4065a94d6bf1888911c205240aca565f8db4
Harshit Bhatt <harshitbhatt972@gmail.com>
2021-05-21 18:12:56 +0530

    done

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const { ppid } = require("process");
+const app = express();
+const port = 8080;
+
+app.use(express.static(path.join(__dirname, "frontend")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index"));
+})
+
+app.post("/", (req, res) => {
+    txt = fs.readFileSync("query.txt");
+    fs.writeFileSync("query.txt", txt + "\nname: " + req.body.name + " message: " + req.body.message + " number: " + req.body.number, {
+        encoding: "utf-8",
+    });
+    res.redirect("/");
+})
+
+app.listen(port, () => {
+    console.log("Server is up!");
+})
